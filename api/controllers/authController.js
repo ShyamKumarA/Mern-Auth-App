5257285dc0bcdf346b01403126fa337fe39bfaef
ShyamKumarA <shyamkumarbeypore@gmail.com>
2023-12-14 08:09:11 +0530

    signin start

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
 
 export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
@@ -13,4 +14,21 @@ export const signup=async(req,res,next)=>{
         next(error)
     }
     
+}
+
+export const signin=async(req,res,next)=>{
+    const {email,password}=req.body;
+    try{
+        const validUser=await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(401,'User not found'))
+        }
+        const validPassword=bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler(401,'Wrong credentials'))
+        }
+
+    }catch(error){
+        next(error);
+    }
 }
--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -5,5 +5,7 @@ const router=express.Router()
 
 
 router.post("/signup",signup)
+router.post("/signin",signup)
+
 
 export default router;
